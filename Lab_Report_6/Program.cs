ea0e7a97dd0597b245424bef59165165797e4354
[Ashna Shrestha] <[sthashna226@gmail.com]>
2026-08-06 17:12:18 +0545

    Lab-6

--- Lab_Report_6/Program.cs
+++ Lab_Report_6/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to .NET CLI");
+Console.WriteLine("--------------------");
+Console.WriteLine("This application demonstrates the .NET CLI.");
+Console.WriteLine();
+Console.WriteLine("Compilation: Source Code -> MSIL -> CLR -> Machine Code");
+Console.WriteLine();
+Console.WriteLine("Application executed successfully.");
