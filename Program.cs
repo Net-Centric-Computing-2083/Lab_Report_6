d2ec6765b93573b2651cb4e2e7e4a9a124d1dfae
ananta23-droid <anantacsit23@oic.edu.np>
2026-08-06 10:41:48 +0545

    Add Lab6

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Welcome to .NET CLI");
+Console.WriteLine("--------------------");
+Console.WriteLine("This application demonstrates the .NET CLI.");
+Console.WriteLine();
+Console.WriteLine("Compilation: Source Code -> MSIL -> CLR -> Machine Code");
+Console.WriteLine();
+Console.WriteLine("Application executed successfully.");
