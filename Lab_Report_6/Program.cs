f9e2c19806f85db84297e7d0962db253ab288055
Saurav Basnet <misterbasnet@gmail.com>
2026-08-06 20:48:10 +0545

    Lab 6

--- Lab_Report_6/Program.cs
+++ Lab_Report_6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+Console.WriteLine("Lab Report 6: Building and Executing .NET Core Applications");
 Console.WriteLine("Building and Executing .NET Core Applications");
 Console.WriteLine("Using .NET CLI");
 Console.WriteLine("Application executed successfully.");
