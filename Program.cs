adbedb6f4ae63b4a30dd8ea711e226089f62f43e
Samikshya Saud <12345678+SamikshyaSaud@users.noreply.github.com>
2026-08-07 00:02:14 +0545

    Complete Lab Report 6

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab_Report_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to .NET CLI");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine(".NET CLI Commands Demonstration");
+            Console.WriteLine();
+            Console.WriteLine("Compilation: Successful");
+            Console.WriteLine("Execution: Successful");
+            Console.WriteLine("CLR Loaded Successfully");
+            Console.WriteLine("MSIL Executed Successfully");
+            Console.WriteLine();
+            Console.WriteLine("Lab Report 6 Completed Successfully.");
+        }
+    }
+}
