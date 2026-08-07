594a5399e219a9d6eca11888e32390155ee745bd
Aadarsha Gautam <aadarshacsit23@oic.edu.np>
2026-08-07 07:50:27 +0545

    Added Lab 6

--- Program.cs
+++ Program.cs
@@ -1,21 +1,7 @@
-﻿using System;
-
-namespace Lab_Report_6
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to .NET CLI");
-            Console.WriteLine("----------------------------");
-            Console.WriteLine(".NET CLI Commands Demonstration");
-            Console.WriteLine();
-            Console.WriteLine("Compilation: Successful");
-            Console.WriteLine("Execution: Successful");
-            Console.WriteLine("CLR Loaded Successfully");
-            Console.WriteLine("MSIL Executed Successfully");
-            Console.WriteLine();
-            Console.WriteLine("Lab Report 6 Completed Successfully.");
-        }
-    }
-}
+﻿Console.WriteLine("Welcome to .NET CLI");
+Console.WriteLine("--------------------");
+Console.WriteLine("This application demonstrates the .NET CLI.");
+Console.WriteLine();
+Console.WriteLine("Compilation: Source Code -> MSIL -> CLR -> Machine Code");
+Console.WriteLine();
+Console.WriteLine("Application executed successfully.");
