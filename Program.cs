32247cd79940b1c9dcc27c758f30ecea2c290470
Aadarsha <aadarshakhadka710@gmail.com>
2026-08-08 21:29:14 +0545

    Lab 6: Commit

--- Program.cs
+++ Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 
-namespace Lab_Report_6
+namespace HelloWorldApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to .NET CLI");
-            Console.WriteLine("----------------------------");
-            Console.WriteLine(".NET CLI Commands Demonstration");
-            Console.WriteLine();
-            Console.WriteLine("Compilation: Successful");
-            Console.WriteLine("Execution: Successful");
-            Console.WriteLine("CLR Loaded Successfully");
-            Console.WriteLine("MSIL Executed Successfully");
-            Console.WriteLine();
-            Console.WriteLine("Lab Report 6 Completed Successfully.");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to .NET Core Application");
+
+            Console.WriteLine("Application executed successfully using .NET CLI.");
         }
     }
 }
